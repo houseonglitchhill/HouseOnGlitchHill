1959d2f3dc208b18e1a7626457e988bf27376fed
Cody Regalado <coregalado@csumb.edu>
2019-04-16 14:11:30 -0700

    creation of CodyBranch

---
+++ Assets/CodyTestFolder/EventBSOD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBSOD : MonoBehaviour
+{
+
+    public Canvas bsod;
+    public Canvas playerUI;
+    public AudioSource[] allAudioInScene;
+
+
+
+    private void toggleBSOD()
+    {
+        bsod.enabled = !bsod.enabled;
+        playerUI.enabled = !playerUI.enabled;
+
+        foreach (AudioSource audioSource in allAudioInScene)
+        {
+            audioSource.GetComponent<AudioSource>().enabled = !audioSource.GetComponent<AudioSource>().enabled;
+        }
+    }
+
+}
