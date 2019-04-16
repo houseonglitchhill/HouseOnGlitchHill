bbe2ae4dd292105a452d019d719f01bc85cc70dc
Jessica Ubaldo <jubaldo@csumb.edu>
2019-04-16 15:38:09 -0700

    Work on Door object

--- Assets/TestMove.cs
+++ Assets/TestMove.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestMove : MonoBehaviour {
+    Rigidbody rb;
+    float h, v, speed;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+        h = 0;
+        v = 0;
+        speed = 0.2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        transform.Translate(speed * h, 0, speed * v);
 	}
 }
