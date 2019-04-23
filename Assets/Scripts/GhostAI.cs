67676b90e13594b9d4d7f97313aeea2a28e0679e
Natalie <ngray@csumb.edu>
2019-04-23 07:36:30 -0700

    Ghost Model and AI

---
+++ Assets/Scripts/GhostAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostAI : MonoBehaviour {
+    private NavMeshAgent agent;
+    public GameObject Player, OrbOne, OrbTwo, OrbThree;
+    private Transform target;
+    private float distanceToTarget, coolOffTimer;
+    public bool engaging;
+    public float coolOffTime, closeDistance;
+    
+    // Use this for initialization
+    void Start () {
+        if (Player == null) {
+            GameObject[] t = GameObject.FindGameObjectsWithTag("Player");
+            target = t[0].transform;
+        } else {
+            target = Player.transform;
+        }
+        agent = GetComponent<NavMeshAgent>();
+        engaging = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (engaging) {
+            agent.isStopped = false;
+            OrbOne.GetComponent<GhostOrbController>().baseColor = Color.red;
+            OrbThree.GetComponent<GhostOrbController>().baseColor = Color.red;
+            OrbTwo.GetComponent<GhostOrbController>().baseColor = Color.red;
+        }
+        else {
+            coolOffTimer += Time.deltaTime;
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
+            OrbOne.GetComponent<GhostOrbController>().baseColor = Color.white;
+            OrbThree.GetComponent<GhostOrbController>().baseColor = Color.white;
+            OrbTwo.GetComponent<GhostOrbController>().baseColor = Color.white;
+        }
+        agent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget < closeDistance) {
+            engaging = false;
+            coolOffTimer = 0;
+        }
+        if(coolOffTimer > coolOffTime) {
+            engaging = true;
+        }
+    }
+}
---
+++ Assets/Scripts/GhostOrbController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostOrbController : MonoBehaviour {
+    public float speed, colorSpeed;
+    public GameObject centerPoint;
+    public Vector3 axis;
+    public Color baseColor;
+    private Renderer Orbrenderer;
+    private Material OrbMaterial;
+    // Use this for initialization
+
+    void Start() {
+        Orbrenderer = GetComponent<Renderer>();
+        OrbMaterial = Orbrenderer.material;
+        baseColor = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float floor = 0.3f;
+        float ceiling = 1.0f;
+        float emission = Mathf.PingPong(Time.time * colorSpeed, ceiling - floor);
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+
+        OrbMaterial.SetColor("_EmissionColor", finalColor);
+        transform.RotateAround(centerPoint.transform.position, axis, speed * Time.deltaTime);
+    }
+}
