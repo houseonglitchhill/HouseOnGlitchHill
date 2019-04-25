388b42eb872d6cdf96dee5135b2b9a66c0ae1ce6
Cody Regalado <coregalado@csumb.edu>
2019-04-24 17:32:32 -0700

    completed functionality of CrashEvent

--- Assets/CodyTestFolder/EventBSOD.cs
+++ Assets/CodyTestFolder/EventBSOD.cs
@@ -9,16 +9,42 @@ public class EventBSOD : MonoBehaviour
     public Canvas playerUI;
     public AudioSource[] allAudioInScene;
 
+    private void Start()
+    {
+        bsod.enabled = false;
+    }
 
+    IEnumerator TriggerBSOD()
+    {
+        
+        toggleBSOD();
+        Time.timeScale = 0.01f;
+        yield return new WaitForSeconds(0.05f);
+        toggleBSOD();
+        Time.timeScale = 1f;
+        Destroy(this.gameObject);
+    }
 
     private void toggleBSOD()
     {
-        bsod.enabled = !bsod.enabled;
+        //bsod.enabled = !bsod.enabled;
         playerUI.enabled = !playerUI.enabled;
 
         foreach (AudioSource audioSource in allAudioInScene)
         {
             audioSource.GetComponent<AudioSource>().enabled = !audioSource.GetComponent<AudioSource>().enabled;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger activated");
+
+        if(other.gameObject.name == "Player")
+        {
+            Debug.Log("Player Detected");
+
+            StartCoroutine(TriggerBSOD());
         }
     }
 
