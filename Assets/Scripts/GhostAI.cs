51bf9aeecdf9eb886cbe99c81394217bd7499905
Natalie Gray <shed9248@csumb.ad.csumb.edu>
2019-04-25 12:19:57 -0700

    added some comments

--- Assets/Scripts/GhostAI.cs
+++ Assets/Scripts/GhostAI.cs
@@ -3,61 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GhostAI : MonoBehaviour {
-    private NavMeshAgent agent;
+public class GhostAI : MonoBehaviour
+{
+    //References to other objects
     public GameObject Player, OrbOne, OrbTwo, OrbThree;
     private GhostOrbController orbControllOne, orbControllTwo, orbControllThree;
+
+    public Color engagedColor, inactiveColor;
+
+    public bool engaging; // Whether Ghost is pursuing player
+    public float coolOffTime, closeDistance; //Cooldown timer and how close the ghost should be to the player before disengaging
+
+    //Navmesh agent stuff
+    private NavMeshAgent agent;
     private Transform target;
     private float distanceToTarget, coolOffTimer;
-    public bool engaging;
-    public float coolOffTime, closeDistance;
-    
+
+
     // Use this for initialization
-    void Start () {
-        if (Player == null) {
+    void Start()
+    {
+        // Get a reference to the player object
+        if (Player == null)
+        {
             GameObject[] t = GameObject.FindGameObjectsWithTag("Player");
             target = t[0].transform;
-        } else {
+        }
+        else
+        {
             target = Player.transform;
         }
-        agent = GetComponent<NavMeshAgent>();
+
+        agent = GetComponent<NavMeshAgent>(); //setup the nav agent
         engaging = true;
+        //Get references to the color control on orbs
         orbControllOne = OrbOne.GetComponent<GhostOrbController>();
         orbControllTwo = OrbTwo.GetComponent<GhostOrbController>();
         orbControllThree = OrbThree.GetComponent<GhostOrbController>();
+
+        //Define default colors
+        engagedColor = Color.red;
+        inactiveColor = Color.white;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (engaging) {
-            agent.isStopped = false;
-            orbControllOne.baseColor = Color.red;
-            orbControllTwo.baseColor = Color.red;
-            orbControllThree.baseColor = Color.red;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (engaging)
+        { // Runs when ghost is pursing player
+            agent.isStopped = false; // Starts the nav agent
+            orbControllOne.baseColor = engagedColor;
+            orbControllTwo.baseColor = engagedColor;
+            orbControllThree.baseColor = engagedColor;
             orbControllOne.SetSpeed("High");
             orbControllTwo.SetSpeed("High");
             orbControllThree.SetSpeed("High");
 
 
         }
-        else {
+        else
+        { // Runs when the ghost is inactive
             coolOffTimer += Time.deltaTime;
-            agent.isStopped = true;
+            agent.isStopped = true; // Stops the nav agent
             agent.velocity = Vector3.zero;
-            orbControllOne.baseColor = Color.white;
-            orbControllTwo.baseColor = Color.white;
-            orbControllThree.baseColor = Color.white;
+            orbControllOne.baseColor = inactiveColor;
+            orbControllTwo.baseColor = inactiveColor;
+            orbControllThree.baseColor = inactiveColor;
             orbControllOne.SetSpeed("Low");
             orbControllTwo.SetSpeed("Low");
             orbControllThree.SetSpeed("Low");
         }
-        agent.SetDestination(target.position);
-        distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget < closeDistance) {
+
+        agent.SetDestination(target.position); // Sets nav agent target to player position
+        distanceToTarget = Vector3.Distance(target.position, transform.position); //Store the distance to the target
+
+        //If the ghost has reached the player make it inactive for a peroid
+        if (distanceToTarget < closeDistance)
+        {
             engaging = false;
             coolOffTimer = 0;
         }
-        if(coolOffTimer > coolOffTime) {
+        if (coolOffTimer > coolOffTime)
+        {
             engaging = true;
         }
     }
--- Assets/Scripts/GhostOrbController.cs
+++ Assets/Scripts/GhostOrbController.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostOrbController : MonoBehaviour {
+public class GhostOrbController : MonoBehaviour
+{
+
     public float lowSpeed, highSpeed, currentSpeed, colorSpeed;
     public GameObject centerPoint;
     public Vector3 axis;
     public Color baseColor;
 
+    //References
     private Renderer Orbrenderer;
     private Material OrbMaterial;
     private ParticleSystem ps;
-    
+
     // Use this for initialization
 
-    void Start() {
+    void Start()
+    {
         Orbrenderer = GetComponent<Renderer>();
         OrbMaterial = Orbrenderer.material;
         baseColor = Color.red;
@@ -23,26 +27,29 @@ public class GhostOrbController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+
+        //Math to cause the orbs color to pulse
         float floor = 0.5f;
         float ceiling = 0.8f;
         float emission = Mathf.PingPong(Time.time * colorSpeed, ceiling - floor);
 
         Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
 
-        OrbMaterial.SetColor("_EmissionColor", finalColor);
-        var main = ps.main;
-        main.startColor = finalColor;
-        transform.RotateAround(centerPoint.transform.position, axis, currentSpeed * Time.deltaTime);
+        OrbMaterial.SetColor("_EmissionColor", finalColor); //Sets the orb to the color calculated
+        var main = ps.main; // Get a reference to the particle system
+        main.startColor = finalColor; // set particle colors
+        transform.RotateAround(centerPoint.transform.position, axis, currentSpeed * Time.deltaTime); // Causes orbs to rotate the center point
     }
 
-    public void SetSpeed(string speed)
+    public void SetSpeed(string speed) // Function to allow other scripts to change the speed of ghost animations
     {
-        if(speed == "High")
+        if (speed == "High")
         {
             currentSpeed = highSpeed;
         }
-        if(speed == "Low")
+        if (speed == "Low")
         {
             currentSpeed = lowSpeed;
         }
