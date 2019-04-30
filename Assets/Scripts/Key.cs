ecc9276b8498f5a5e1f742f1c318103f0b765454
Jessica Ubaldo <jubaldo@csumb.edu>
2019-04-30 12:14:53 -0700

    added a painting

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour {
+    const int numOfKey;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
