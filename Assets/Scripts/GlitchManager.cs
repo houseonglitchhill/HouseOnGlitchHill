f827f8fadb860f9b0fc4c36ea820151bd1bd16e0
Cody Regalado <coregalado@csumb.edu>
2019-05-13 16:16:30 -0700

    adding windows event, prototyped GlitchManager

--- Assets/Scripts/GlitchManager.cs
+++ Assets/Scripts/GlitchManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GlitchManager : MonoBehaviour {
+    public GameObject bsodEvent, crashEvent, windowsEvent, staticEvent;
     public float intialDelay, interGlitchDelay, intialRandomOffset, interRandomOffset;
     private float intialTimer, nextGlitchTimer, timeToFirstGlitch, timeToNextGlitch;
-    private bool bsod, crash, intialStart, firstGlitch;
+    private bool bsod, crash, windows, intialStart, firstGlitch;
+
     // Use this for initialization
     void Start() {
-        bsod = crash = intialStart = firstGlitch = false;
+        bsod = crash = windows = intialStart = firstGlitch = false;
         intialTimer = nextGlitchTimer = 0f;
         timeToFirstGlitch = intialDelay + Random.Range(-intialRandomOffset, intialRandomOffset);
         timeToNextGlitch = 50f;
@@ -38,26 +40,138 @@ public class GlitchManager : MonoBehaviour {
             timeToNextGlitch = interGlitchDelay + Random.Range(-interRandomOffset, interRandomOffset);
         }
     }
+
+    #region EventCalling
     private void SelectAndTrigger() {
         Debug.Log("Glitch");
-        int randomRoll;
-        if (!bsod && !crash) {
-            randomRoll = Random.Range(0, 100);
-        } else if (!bsod) {
-            randomRoll = Random.Range(0, 90);
-        } else {
-            randomRoll = Random.Range(0, 80);
-        }
-
-        if (randomRoll > 90) {
-            // Start Crash glitch
-        } else if (randomRoll > 80) {
-            //Start BSOD glitch
-        } else {
-            //Start Static Glitch
+        if (!bsod && !crash && !windows) {
+            chooseFromFourEvents();
+        } else if ((bsod && !crash && !windows) || (!bsod && crash && !windows) || (!bsod && !crash && windows)) {
+            chooseFromThreeEvents();
+        } else if((bsod && !crash && windows) || (!bsod && crash && !windows) || (bsod && !crash && !windows)) {
+            chooseFromTwoEvents();
+        } else
+        {
+            callStaticEvent();
         }
     }
+
+    //This function randomly picks one of any of the events
+    private void chooseFromFourEvents()
+    {
+        int randomRoll = Random.Range(0, 100);
+        if (randomRoll > 90)
+        {
+            callBsodEvent();
+        }
+        else if (randomRoll > 80)
+        {
+            callCrashEvent();
+        }
+        else if (randomRoll > 70)
+        {
+            callWindowsEvent();
+        }
+        else
+        {
+            callStaticEvent();
+        }
+    }
+
+    //This function chooses an event dependent on which event has already been activated
+    private void chooseFromThreeEvents()
+    {
+        int randomRoll = Random.Range(0, 90);
+        if(randomRoll > 70)
+        {
+            if (bsod)
+            {
+                if(randomRoll > 80)
+                {
+                    callCrashEvent();
+                }
+                else
+                {
+                    callWindowsEvent();
+                }
+            } else if (crash)
+            {
+                if (randomRoll > 80)
+                {
+                    callBsodEvent();
+                }
+                else
+                {
+                    callWindowsEvent();
+                }
+            }
+            else {
+                if (randomRoll > 80)
+                {
+                    callBsodEvent();
+                }
+                else
+                {
+                    callCrashEvent();
+                }
+            }
+        } else
+        {
+            callStaticEvent();
+        }
+
+    }
+
+    //This function chooses an event dependent on which event hasn't been activated
+    private void chooseFromTwoEvents()
+    {
+        int randomRoll = Random.Range(0, 80);
+        if (randomRoll > 70)
+        {
+            if (!bsod)
+            {
+                callBsodEvent();
+            }
+            else if (!crash)
+            {
+                callCrashEvent();
+            }
+            else
+            {
+                callWindowsEvent();
+            }
+        }
+        else
+        {
+            callStaticEvent();
+        }
+    }
+    #endregion
+
+    #region EventCalls
+    private void callBsodEvent()
+    {
+        bsod = true;
+    }
+
+    private void callCrashEvent()
+    {
+        crash = true;
+    }
+
+    private void callWindowsEvent()
+    {
+        windows = true;
+    }
+
+    private void callStaticEvent()
+    {
+
+    }
+    #endregion
+
     public void BeginGlitchTimer() {
         intialStart = true;
     }
+
 }
